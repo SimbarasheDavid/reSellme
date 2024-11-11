1c394c143944365d06c29c1b97c841cbafb83c0a
SimbarasheDavid <simbarashedavidmurahwa@gmail.com>
2024-11-11 11:27:47 +0200

    first-commit

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -8,8 +8,15 @@ import { useState } from 'react';
 import ham from '../assets/images/bars-solid.svg'
 import { RxHamburgerMenu } from 'react-icons/rx'
 
+import '../styles/ham.scss'
 const NavBar = () => {
     const [active, setActive] = useState(false);
+    function toggleModal() {
+        const modal = document.getElementById('modal');
+        const overlayo = document.querySelector('.overlayo');
+        modal.classList.toggle('open');
+        overlayo.classList.toggle('active');
+    }
     return (
         <div id='nav-bar'>
             <nav>
@@ -44,18 +51,56 @@ const NavBar = () => {
                         </div>
                     </div>
                     <div className="dark">
-                        <Darkmode onClick={() => {setActive(!active)}}/>
+                        <Darkmode onClick={() => { setActive(!active) }} />
                     </div>
                     <div className="search">
                         <li><img src={search} alt="" /></li>
                         <li>Search</li>
                     </div>
-                    
+
                 </ul>
                 <ul>
-                <button className='hamburger'><RxHamburgerMenu className='menu'/></button>
+                    <button className='hamburger' onClick={() => { toggleModal() }}><RxHamburgerMenu className='menu' /></button>
+                    <div className="overlayo active" onClick={() => { toggleModal() }}></div>
+                    <div className="modal" id='modal'>
+                    <span class="close"  onClick={() => { toggleModal() }}>&times;</span>
+                        <h2>Menu</h2>
+                        <div className="logo">
+                        <img src={logo} alt="" />
+                        <div className="nav-links">
+                        <li>Docs</li>
+                        <li>Blog</li>
+                        <li>About-us</li>
+                        <div className='suport'>
+                        <li>Support</li>
+                        <div className="sup-img">
+                            <img src={port} alt="" />
+                        </div>
+                    </div>
+                    <div className='portal'>
+                        <li>Portal</li>
+                        <div className="portal-img">
+                            <img src={port} alt="" />
+                        </div>
+                    </div>
+                    <div className='Register'>
+                        <li>Support</li>
+                        <div className="reg-img">
+                            <img src={port} alt="" />
+                        </div>
+                    </div>
+                    <div className="dark">
+                        <Darkmode onClick={() => { setActive(!active) }} />
+                    </div>
+                    <div className="search">
+                        <li><img src={search} alt="" /></li>
+                        <li>Search</li>
+                    </div>
+                    </div>
+                    </div>
+                    </div>
                 </ul>
-           
+
             </nav>
         </div>
     )
